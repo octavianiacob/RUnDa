d3d1943e41190e76a1e3a2d8e63a6f5897107e7d
stefichim <ichim.stefan99@gmail.com>
2020-05-22 01:17:22 +0300

    added lineChart with small bugs

--- ajax.js
+++ ajax.js
@@ -84,12 +84,13 @@ function onClick(e) {
                 console.error(error);
             })
 
-        diagrama(url);
+      //barChart(url);
+        lineChart(url)
 
     }
   
 }
-function diagrama(url)
+function barChart(url)
 {
     const {
         select,
@@ -178,3 +179,111 @@ function diagrama(url)
 
     d3.selectAll("svg > *").remove(); //ca sa imi stearga chart-ul inainte de alt apel
 }
+
+
+function lineChart(url){
+    const {
+        select,
+        scaleLinear,
+        extent,
+        axisLeft,
+        axisBottom,
+        line,
+        curveBasis,
+        json,
+        scaleBand
+      } =d3;
+      
+      const svg = select('svg');
+      
+      const width = +svg.attr('width');
+      const height = +svg.attr('height');
+      
+      const render = data => {
+        const title = 'A Week in San Francisco';
+        
+        const xValue = d => d.month;
+        const xAxisLabel = 'Time';
+        
+        const yValue = d => d[getColumn];
+        const circleRadius = 6;
+        const yAxisLabel = '';
+        
+        const margin = { top: 60, right: 40, bottom: 88, left: 105 };
+        const innerWidth = width - margin.left - margin.right;
+        const innerHeight = height - margin.top - margin.bottom;
+        
+        const xScale = scaleBand()
+        .domain(data.map(xValue))//d inseamna 1 row
+        .range([0, innerWidth])
+
+    
+    
+      const yScale = scaleLinear()
+        .domain(extent(data, yValue))
+        .range([innerHeight, 0])
+        .nice();
+    
+        
+        const g = svg.append('g')
+          .attr('transform', `translate(${margin.left},${margin.top})`);
+        
+        const xAxis = axisBottom(xScale)
+          .tickSize(-innerHeight)
+          .tickPadding(15);
+        
+        const yAxis = axisLeft(yScale)
+          .tickSize(-innerWidth)
+          .tickPadding(10);
+        
+        const yAxisG = g.append('g').call(yAxis);
+        yAxisG.selectAll('.domain').remove();
+        
+        yAxisG.append('text')
+            .attr('class', 'axis-label')
+            .attr('y', -60)
+            .attr('x', -innerHeight / 2)
+            .attr('fill', 'black')
+            .attr('transform', `rotate(-90)`)
+            .attr('text-anchor', 'middle')
+            .text(yAxisLabel);
+        
+        const xAxisG = g.append('g').call(xAxis)
+          .attr('transform', `translate(0,${innerHeight})`);
+        
+        xAxisG.select('.domain').remove();
+        
+        xAxisG.append('text')
+            .attr('class', 'axis-label')
+            .attr('y', 80)
+            .attr('x', innerWidth / 2)
+            .attr('fill', 'black')
+            .text(xAxisLabel);
+        
+        const lineGenerator = line()
+          .x(d => xScale(xValue(d)))
+          .y(d => yScale(yValue(d)))
+          .curve(curveBasis);
+        
+        g.append('path')
+            .attr('class', 'line-path')
+            .attr('d', lineGenerator(data));
+        
+        g.append('text')
+            .attr('class', 'title')
+            .attr('y', -10)
+            .text(title);
+      };
+      
+      json(url)
+        .then(data => {
+          data.forEach(d => {
+              console.log(getColumn)
+            d.getColumn = +d.getColumn;
+            
+          });
+          render(data);
+        });
+}
+
+lineChart();
