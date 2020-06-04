9f075a71a53cba22d495fce17c032df08158a5e7
stefichim <ichim.stefan99@gmail.com>
2020-06-04 14:40:32 +0300

    pdf_js_CodeAdded(notFinal)

--- js/ajax.js
+++ js/ajax.js
@@ -5,6 +5,7 @@ let getYear = ""
 let getCsv = ""
 let url = ""
 let urlForCsv = ""
+let getPdf = ""
 
 let containerElement = document.querySelector("#container");
 
@@ -60,6 +61,7 @@ if (buttonElement)
 //-------------------------------------------------------SELECTARE FILTRE+GENERARE DIAGRAMA----------------------------------------------------------
 
 function onClick(e) {
+
   if (e.target.tagName == 'INPUT') {
     d3.selectAll("svg > *").remove();
     if (e.target.hasAttribute("data-getTableName")) {
@@ -119,6 +121,20 @@ function onClick(e) {
     if (!(getCountyName === "" || getTableName === "" || getColumn === "" || getYear === "")) {
       url = `/RunDa/api/counties/${getCountyName}?filtered_by=${getTableName}&year=${getYear}&sorted_by=month`
 
+
+
+      fetch(url)
+      .then(function(resp){
+        return resp.json();
+      })
+      .then(function (jsonResp){
+        console.log("dadada "+jsonResp)
+      })
+      .catch(function(){
+        //error
+      })
+
+
       // if (getColumn != "")
       //   barChart(url);
 
@@ -129,6 +145,7 @@ function onClick(e) {
       //   pieChart(url);
 
     }
+
   }
 
 }
@@ -140,6 +157,9 @@ function onClickExport(e) {
 
     if (e.target.hasAttribute("data-getCsv")) {
       getCsv = e.target.getAttribute("data-getCsv")
+    }
+    if (e.target.hasAttribute("data-getPdf")) {
+      getPdf = e.target.getAttribute("data-getPdf")
     }
 
 
@@ -205,8 +225,63 @@ function onClickExport(e) {
       getReport();
     }
 
+    //pdf??
+
+    var json = '[["Customer Id","Name","Country"],[1,"John Hammond","United States"],[2,"Mudassar Khan","India"],[3,"Suzanne Mathews","France"],[4,"Robert Schidner","Russia"]]';
+
+    //Convert JSON string to JSON object.
+    var customers = eval(json);
+
+    //Convert JSON to HTML Table.
+    var table = document.createElement("TABLE");
+    table.border = "1";
+    table.Id = "tblCustomers";
+
+    //Get the count of columns.
+    var columnCount = customers[0].length;
+
+    //Add the header row.
+    var row = table.insertRow(-1);
+    for (var i = 0; i < columnCount; i++) {
+      var headerCell = document.createElement("TH");
+      headerCell.innerHTML = customers[0][i];
+      row.appendChild(headerCell);
+    }
+
+    //Add the data rows.
+    for (var i = 1; i < customers.length; i++) {
+      row = table.insertRow(-1);
+      for (var j = 0; j < columnCount; j++) {
+        var cell = row.insertCell(-1);
+        cell.innerHTML = customers[i][j];
+      }
+    }
+
+    //Append the Table to the HTML DIV.
+    var dvTable = document.getElementById("dvTable");
+    dvTable.innerHTML = "";
+    dvTable.appendChild(table);
+
+
+    //Convert Table to PDF.
+    html2canvas(document.getElementById('dvTable'), {
+      onrendered: function (canvas) {
+        var data = canvas.toDataURL();
+        var docDefinition = {
+          content: [{
+            image: data,
+            width: 500
+          }]
+        };
+        pdfMake.createPdf(docDefinition).download("JSON.pdf");
+
+        //Remove the Table.
+        dvTable.innerHTML = "";
+      }
+    });
   }
 }
+
 
 
 function barChart(url) {
@@ -577,3 +652,9 @@ function pieChart(url) {
 
   d3.selectAll("svg > *").remove();
 }
+
+// function myFunction() {
+//   var btn = document.createElement("BUTTON");
+//   btn.innerHTML = "CLICK ME";
+//   document.body.appendChild(btn);
+// }
